92b9fe149912d7ab5971abe667ae76e679e4c24d
KhatoBurduli <khatuna.burduli05@gmail.com>
2024-06-25 21:27:57 +0400

    did user list

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import UsersList from './components/UsersList';
+const React = require("react");
+const { Component } = React;
+
+class App extends Component {
+
+ 
+
+  render() {
+    return (
+    <UsersList />
+
+    );
+  }
+}
+
+export default App;
